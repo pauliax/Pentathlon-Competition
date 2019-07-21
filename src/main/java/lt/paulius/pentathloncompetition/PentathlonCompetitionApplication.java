cafee828b4173e4c0b9a24b53abca2227d8fd697
pauliax <pauliax6@gmail.com>
2019-07-21 11:59:52 +0300

    Spring boot starter project

---
+++ src/main/java/lt/paulius/pentathloncompetition/PentathlonCompetitionApplication.java
@@ -0,0 +1,13 @@
+package lt.paulius.pentathloncompetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PentathlonCompetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PentathlonCompetitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lt/paulius/pentathloncompetition/PentathlonCompetitionApplicationTests.java
@@ -0,0 +1,16 @@
+package lt.paulius.pentathloncompetition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PentathlonCompetitionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
